e762861069bd613301eba2988659723f68e53183
analayze <annalisa@antonioli.dev>
2025-05-25 15:20:20 +0200

    Add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,27 @@ function validateField(input) {
   input.classList.toggle('error', !isValid)
   return isValid
 }
+
+const navOpener = document.querySelector('.navOpener')
+const navCloser = document.querySelector('.navCloser')
+
+if (navOpener) {
+  const nav = document.querySelector('header nav')
+  navOpener.addEventListener('click', () => {
+    nav.classList.add('is-open')
+  })
+  navCloser.addEventListener('click', () => {
+    nav.classList.remove('is-open')
+  })
+}
+
+// Get current page to style li item
+const currentPage = window.location.pathname.replace('/', '')
+const navItems = document.querySelectorAll('header nav li')
+Array.from(navItems).map((item) => {
+  const a = item.querySelector('a')
+  const splitA = a.href.split('/')
+  splitA[splitA.length - 1] === currentPage
+    ? item.classList.add('current')
+    : item.classList.remove('current')
+})
